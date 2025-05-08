208cced2c460d335332b94b1aac047ee6cde853d
Anderson <suporte.truesoft@gmail.com>
2025-05-08 16:07:33 -0300

    att

--- sw.js
+++ sw.js
@@ -1,3 +1,39 @@
-self.addEventListener("install", e => {
-  console.log("Service Worker instalado");
+const CACHE_NAME = 'powergestor-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('powergestor.com')) {
+    return fetch(event.request);
+  }
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
