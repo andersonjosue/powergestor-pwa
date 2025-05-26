8d097deb56ed9bd5579ee3714c29e46d67856fbd
Anderson <suporte.truesoft@gmail.com>
2025-05-26 15:51:31 -0300

    att

--- planilhas/sw.js
+++ planilhas/sw.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   '/index.html',
   '/style.css',
   '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '../icons/icon-192x192.png',
+  '..//icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
