cc86db54f19a0b0e433b5083a27fbe9b2b54d32a
Anderson <suporte.truesoft@gmail.com>
2025-06-05 14:37:17 -0300

    Criação powerchats

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Powergestor";
 import NotFound from "./pages/NotFound";
 import Planilhas from "./pages/Planilhas";
+import Powerchats from "./pages/Powerchats";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/powergestor" element={<Home />} />
         <Route path="/planilhas" element={<Planilhas />} />
+        <Route path="/powerchats" element={<Powerchats />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
