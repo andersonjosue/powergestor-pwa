3ca4746050311e608d1ffb641b4b35b737d31041
Anderson <andersonjosue@gmail.com>
2025-06-08 17:56:27 +0000

    vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt'],
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}']
+      },
+      // Não defina manifest aqui — você vai usar os externos
+      injectRegister: 'auto'
+    })
+  ]
 });
